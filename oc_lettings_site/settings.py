a28de0708f4ebcc7600c90d82a0639c6a9309bcd
johnchem <j.payssan@gmail.com>
2023-12-27 15:35:44 +0100

    reorganize the templates files

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'oc_lettings_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
