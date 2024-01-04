d821d83ee269038e05ff282cd5f76dfdd96a4194
johnchem <j.payssan@gmail.com>
2024-01-04 11:00:51 +0100

    creation of the tests for the letting app

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,11 +1,56 @@
 import pytest
 from django.urls import reverse, resolve
+from lettings.models import Letting, Address
 
-# Create your tests here.
-@pytest.mark.django_db    
-class TestUrlLettings:
+
+class TestLettingsUrl:
+    """
+    Test for the letting application's urls
+
+    :function: test_letitng_index_url
+    :function: test_letting_detail_url
+    """
+    def test_letting_index_url(self):
+        path = reverse('lettings_index')
+        assert path == "/lettings/"
+        assert resolve(path).view_name == "lettings_index"
+
     def test_letting_detail_url(self):
         path = reverse('letting', kwargs={'letting_id':1})
-        
-        assert path == "/1"
-        assert resolve(path).view_name == "infos"
+        assert path == "/lettings/1/"
+        assert resolve(path).view_name == "letting"
+
+@pytest.mark.django_db
+class TestLettingsModels:
+    def test_create_address(self, client):
+        """
+        test the creation of a new address with correct data
+        """
+        pass
+
+    def test_create_letting(self, client):
+        """
+        test the creation of a new letting with correct data
+        """
+        pass
+
+    def test_validator_address(self, client):
+        """
+        test the error message when incorrect data are provided
+        **data : **
+            number [int] : 0> int > 9999
+            state [str] : length = 2
+            zip_code [int] : 0> int > 9999
+            country_iso_code [str] : length = 3
+        """
+        pass
+
+    def test_delete_letting(self, client):
+        """
+        Test the deletion of a letting, this must also delete the linked address
+        """
+        pass
+
+class TestLettingsView:
+    pass
+
--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,62 +1,10 @@
-from django.http import HttpResponseServerError, Http404
-from django.test import SimpleTestCase, override_settings
-from django.urls import path
-
-def page_not_found_view(request):
-    """
-    Testing view raising the error 404
-    """
-    raise Http404
-
-def server_error_view(request):
-    """
-    Testing view raising the error 500
-    """
-    raise HttpResponseServerError(request, status = 500)
-
-# url for testing purpose
-urlpatterns = [
-    path("404/", page_not_found_view),
-    path("500/", server_error_view),
-]
-
-handler404 = "oc_lettings_site.views.page_not_found"
-handler500 = "oc_lettings_site.views.server_error"
-
-@override_settings(ROOT_URLCONF="oc_lettings_site.urls")
-class CustomErrorHandlerTests(SimpleTestCase):
-    """
-    Class intended to test the error handling
-
-    :function: test_handler_render_404_response()
-        Call the url 404/ and expect a response with a code 400 and a defined message
-    :function: test_handler_render_500_response()
-        Call the url 500/ and and expect a response with a code 500 and a defined message
-    """
-    def test_handler_render_404_response(self):
-        """Test the page received after raising an error 404"""
-        response = self.client.get("404/")
-        self.assertContains(response, "We’re sorry, but the requested page could not be found.", status_code=404)
-
-    def test_handler_render_500_response(self):
-        """Test the page received after raising an error 500"""
-        response = self.client.get("/500/")
-        self.assertContains(response, "There’s been an error.", status_code=500)
-from django.shortcuts import render
 import pytest
 import re
 from bs4 import BeautifulSoup
-from django.http import HttpResponseServerError, Http404, HttpResponse
-from django.test import SimpleTestCase, override_settings
-from django.core.exceptions import ObjectDoesNotExist
-from django.urls import include, path
-from django.urls import reverse, resolve
-from django.test import Client
+from django.http import Http404
+from django.urls import include, path, reverse, resolve
+from django.shortcuts import render
 
-@pytest.fixture()
-def client():
-    client = Client()
-    return client
 
 def page_not_found_view(request):
     """
@@ -83,11 +31,6 @@ urlpatterns = [
     path("", include('oc_lettings_site.urls'))
 ]
 
-handler404 = "oc_lettings_site.views.page_not_found"
-handler500 = "oc_lettings_site.views.server_error"
-
-@pytest.mark.urls('oc_lettings_site.tests')
-@pytest.mark.django_db()
 class TestCustomErrorHandler:
     """
     Class intended to test the error handling
@@ -118,17 +61,13 @@ class TestCustomErrorHandler:
         assert soup.body.find(string=msg)
 
 ### Test the urls ###
-@pytest.mark.urls('oc_lettings_site.tests')
-@pytest.mark.django_db()
 class TestSiteUrl:
     def test_root_url(self):
         path = reverse('index')
         assert path == "/"
         assert resolve(path).view_name == "index"
 
-
 ### Test the views ###
-@pytest.mark.urls('oc_lettings_site.tests')
 @pytest.mark.django_db()
 class TestSiteView:
     def test_index_view(self, client):
@@ -151,3 +90,10 @@ class TestSiteView:
 
         assert resp.status_code == 302
         assert redirect_url == reverse("admin:login")
+
+    def test_connected_admin_view(self, admin_client):
+        resp = admin_client.get("/admin/")
+        
+        assert resp.status_code == 200
+        assert resp.request["PATH_INFO"] == "/admin/"
+        assert resp.template_name == "admin/index.html"
