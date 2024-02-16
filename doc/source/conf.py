a0e80da30542a50836d6f4ce75627e172f219bcb
johnchem <j.payssan@gmail.com>
2024-02-16 14:09:57 +0100

    fix build doc issue

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,4 +41,4 @@ html_static_path = ['_static']
 
 # -- GraphViz configuration ----------------------------------
 graphviz_output_format = 'svg'
-graphviz_dot="/usr/local/bin/dot"
+# graphviz_dot="/usr/local/bin/dot"
